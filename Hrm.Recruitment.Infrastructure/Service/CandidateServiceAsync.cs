e28b2073c65058257ef69c9f21b5e2e1c80c6a43
unknown <zjw642337320@163.com>
2023-03-05 12:58:02 -0500

    project on windows

---
+++ Hrm.Recruitment.Infrastructure/Service/CandidateServiceAsync.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Hrm.Recruitment.ApplicationCore.Contract.Repository;
+using Hrm.Recruitment.ApplicationCore.Contract.Service;
+using Hrm.Recruitment.ApplicationCore.Entity;
+using Hrm.Recruitment.ApplicationCore.Model.Request;
+using Hrm.Recruitment.ApplicationCore.Model.Response;
+using Hrm.Recruitment.Infrastructure.Repository;
+
+namespace Hrm.Recruitment.Infrastructure.Service
+{
+	public class CandidateServiceAsync : ICandidateServiceAsync
+	{
+        private readonly ICandidateRepositoryAsync candidateRepsoitoryAsync;
+
+        public CandidateServiceAsync(ICandidateRepositoryAsync _candidateRepsoitoryAsync)
+		{
+            candidateRepsoitoryAsync = _candidateRepsoitoryAsync;
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            return candidateRepsoitoryAsync.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<CandidateResponseModel>> GetAllAsync()
+        {
+            var result = await candidateRepsoitoryAsync.GetAllAsync();
+            if (result != null)
+            {
+                return result.ToList().Select(model => new CandidateResponseModel()
+                {
+                    Id = model.Id,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName,
+                    Mobile = model.Mobile,
+                    Email = model.Email,
+                    currentAddress = model.currentAddress,
+                    ResumeUrl = model.ResumeUrl
+                });
+            }
+            return null;
+    }
+
+        public async Task<CandidateResponseModel> GetByIdAsync(int id)
+        {
+            var model = await candidateRepsoitoryAsync.GetByIdAsync(id);
+            if (model != null)
+            {
+                return new CandidateResponseModel()
+                {
+                    Id = model.Id,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName,
+                    Mobile = model.Mobile,
+                    Email = model.Email,
+                    currentAddress = model.currentAddress,
+                    ResumeUrl = model.ResumeUrl
+                };
+            }
+            return null;
+        }
+
+        public Task<int> InsertAsync(CandidateRequestModel model)
+        {
+            Candidate candidate = new Candidate()
+            {
+                FirstName = model.FirstName,
+                MiddleName = model.MiddleName,
+                LastName = model.LastName,
+                Mobile = model.Mobile,
+                Email = model.Email,
+                currentAddress = model.currentAddress,
+                ResumeUrl = model.ResumeUrl
+            };
+            return candidateRepsoitoryAsync.InsertAsync(candidate);
+        }
+
+        public async Task<int> UpdateAsync(CandidateRequestModel model)
+        {
+            Candidate candidate = new Candidate()
+            {
+                Id = model.Id,
+                FirstName = model.FirstName,
+                MiddleName = model.MiddleName,
+                LastName = model.LastName,
+                Mobile = model.Mobile,
+                Email = model.Email,
+                currentAddress = model.currentAddress,
+                ResumeUrl = model.ResumeUrl
+            };
+            return await candidateRepsoitoryAsync.UpdateAsync(candidate);
+        }
+    }
+}
+
